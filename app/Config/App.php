da574dfbf40554f47ce2bfd7e054fd025391f46a
yungga123 <53594401+yungga123@users.noreply.github.com>
2021-07-14 20:08:46 +0800

    Update

--- app/Config/App.php
+++ app/Config/App.php
@@ -23,7 +23,7 @@ class App extends BaseConfig
 	 *
 	 * @var string
 	 */
-	public $baseURL = 'http://localhost/citypanther_landing/public';
+	public $baseURL = 'http://localhost/citypanther_landing/public/';
 
 	/**
 	 * --------------------------------------------------------------------------
@@ -108,7 +108,7 @@ class App extends BaseConfig
 	 *
 	 * @var string
 	 */
-	public $appTimezone = 'America/Chicago';
+	public $appTimezone = 'Asia/Manila';
 
 	/**
 	 * --------------------------------------------------------------------------
